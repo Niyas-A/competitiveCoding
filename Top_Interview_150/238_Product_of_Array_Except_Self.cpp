b4f4df7a6acee44b543a42854c169464fff145d5
Niyas-A <niyasttshr33@gmail.com>
2024-10-07 23:44:55 -0700

    adding 238

---
+++ Top_Interview_150/238_Product_of_Array_Except_Self.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = size(nums), prefix = 1, postfix = 1;
+        vector<int> res(n, 1);
+        for(int i=0;i<n;i++){
+            res[i] = prefix;
+            prefix *= nums[i];
+        }
+        for(int i=n-1;i>=0;i--){
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return res;
+    }
+};
